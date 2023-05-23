511d5b26e2809ac821e27a73698c08090cbb661b
WilsonGoins <wilson.goins@ufl.edu>
2023-05-23 00:37:23 -0400

    I am tired. welcome screen is good, maybe I will just figure out pawns and flipping that stuff later on...

--- main.cpp
+++ main.cpp
@@ -31,78 +31,174 @@ void WelcomeScreen(sf::RenderWindow& window, Images& images, string& user1, stri
     sf::Text welcomeText("Chess", font, 175);
     welcomeText.setFillColor(sf::Color::White);
     welcomeText.setOrigin(welcomeText.getLocalBounds().width / 2.0f - 150, welcomeText.getLocalBounds().height / 2.0f + 50);
-    welcomeText.setPosition(sf::Vector2f(150, 150));
+    welcomeText.setPosition(sf::Vector2f(150, 150));        // put it in the top left
     // draw player 1 prompt
-    sf::Text player1Prompt("Player 1:", font, 40);
+    sf::Text player1Prompt("Player 1:                            ", font, 40);      // lots of extra space so the clickable area is bigger
     player1Prompt.setFillColor(sf::Color::White);
-    player1Prompt.setOrigin(player1Prompt.getLocalBounds().width / 2.0f - 150, player1Prompt.getLocalBounds().height / 2.0f + 50);
-    player1Prompt.setPosition(sf::Vector2f(650, 125));
+    player1Prompt.setPosition(sf::Vector2f(710, 100));
     // draw player 2 prompt
-    sf::Text player2Prompt("Player 2:", font, 40);
+    sf::Text player2Prompt("Player 2:                            ", font, 40);
     player2Prompt.setFillColor(sf::Color::White);
-    player2Prompt.setOrigin(player2Prompt.getLocalBounds().width / 2.0f - 150, player2Prompt.getLocalBounds().height / 2.0f + 50);
-    player2Prompt.setPosition(sf::Vector2f(650, 200));
+    player2Prompt.setPosition(sf::Vector2f(710, 175));
     // player 1 name text
-    sf::Text player1Name("P1", font, 40);
+    string p1name = "P1";
+    sf::Text player1Name(p1name, font, 40);
     player1Name.setFillColor(sf::Color::Black);
-    player1Name.setPosition(sf::Vector2f(890, 55));
-    float player1Width = player1Name.getLocalBounds().width;
+    player1Name.setPosition(sf::Vector2f(880, 100));
     // player 1 cursor
     sf::Text player1Cursor("|", font, 45);
     player1Cursor.setStyle(sf::Text::Bold);
     player1Cursor.setFillColor(sf::Color::Yellow);
-    player1Cursor.setPosition(sf::Vector2f(player1Width + 890, 50));
+    player1Cursor.setPosition(sf::Vector2f(player1Name.getLocalBounds().width + 880, 95));    // should be right after the player1 name
     // player 2 name text
-    sf::Text player2Name("P2", font, 40);
+    string p2name = "P2";
+    sf::Text player2Name(p2name, font, 40);
     player2Name.setFillColor(sf::Color::Black);
-    player2Name.setPosition(sf::Vector2f(890, 130));
-    float player2Width = player2Name.getLocalBounds().width;
+    player2Name.setPosition(sf::Vector2f(880, 175));
     // player 2 cursor
     sf::Text player2Cursor("|", font, 45);
     player2Cursor.setStyle(sf::Text::Bold);
     player2Cursor.setFillColor(sf::Color::Yellow);
-    player2Cursor.setPosition(sf::Vector2f(player2Width + 890, 124));
-
-
-
-    sf::Mouse mouse;
-    sf::Clock clock;
-    bool P1Selected = true;
-    bool visible = true;
-    while (window.isOpen()) {
-        // do these in main loop, so it constantly happens
-        window.draw(welcomeImage);
-        window.draw(welcomeText);
-        window.draw(player1Prompt);
-        window.draw(player2Prompt);
-        window.draw(player1Name);
-        window.draw(player2Name);
-        if (clock.getElapsedTime().asSeconds() >= 0.5f) {
-            visible = !visible;
-            clock.restart();
-        }
-        if ((P1Selected) and (visible)) {
-            window.draw(player1Cursor);
-        } else if ((not P1Selected) and (visible)){
-            window.draw(player2Cursor);
-        }
-        window.display();
-
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-                return;
-            } else if (event.type == sf::Event::MouseButtonPressed) {
-                sf::Vector2i click = mouse.getPosition(window);        // get the mouse position
-                if (player1Name.getGlobalBounds().contains(click.x, click.y)) {
-                    P1Selected = true;
-                } else if (player2Name.getGlobalBounds().contains(click.x, click.y)) {
-                    P1Selected = false;
-                }
+    player2Cursor.setPosition(sf::Vector2f(player2Name.getLocalBounds().width + 880, 170));       // right after player2 name
+    // play button
+    sf::Text playButton("Play", font, 60);
+    playButton.setStyle(sf::Text::Bold);
+    playButton.setFillColor(sf::Color::Yellow);
+    playButton.setPosition(sf::Vector2f(playButton.getLocalBounds().width + 800, 745));       // right after player2 name
+    cout << playButton.getLocalBounds().width << endl;
+    // credits
+    sf::Text credit1("Program Developer: Wilson Goins", font, 30);
+    credit1.setStyle(sf::Text::Bold);
+    credit1.setFillColor(sf::Color::Black);
+    credit1.setPosition(sf::Vector2f(25, 745));       // right after player2 name
+    sf::Text credit2("Program Illustrator: Madeleine Schutz", font, 30);
+    credit2.setStyle(sf::Text::Bold);
+    credit2.setFillColor(sf::Color::Black);
+    credit2.setPosition(sf::Vector2f(25, 800));       // right after player2 name
+    // white pawn
+    sf::Texture whitePawn;
+    whitePawn.loadFromFile("Images/whitePawn.jpg");
+    sf::Sprite wPawn(whitePawn);
+    wPawn.scale(.1, .1);
+    wPawn.setPosition(sf::Vector2f(650, 100));
+    // black pawn
+    sf::Texture blackPawn;
+    blackPawn.loadFromFile("Images/blackPawn.jpg");
+    sf::Sprite bPawn(blackPawn);
+    bPawn.scale(.3, .3);
+    bPawn.setPosition(sf::Vector2f(650, 150));
+    // flip button
+    sf::Texture flipButton;
+    flipButton.loadFromFile("Images/flipButt.jpg");
+    sf::Sprite sprite;
+    sprite.setTexture(flipButton, true); // Enable alpha channel
+    sf::Image image = flipButton.copyToImage();
+    for (unsigned int x = 0; x < image.getSize().x; ++x) {
+        for (unsigned int y = 0; y < image.getSize().y; ++y) {
+            sf::Color pixel = image.getPixel(x, y);
+            // Check if the pixel is white (you may need to adjust the threshold)
+            if (pixel == sf::Color::White) {
+                pixel.a = 0; // Set the alpha channel to 0 (transparent)
+                image.setPixel(x, y, pixel);
             }
         }
     }
+    flipButton.loadFromImage(image);
+    sprite.setTexture(flipButton);
+    sf::Sprite flipButt(flipButton);
+    flipButt.scale(.03, .03);
+    flipButt.setPosition(sf::Vector2f(600, 125));
 
+
+
+
+
+
+    sf::Mouse mouse;        // mouse object to get position of the clicks
+    sf::Clock clock;            // clock to facilitate blinking cursor
+    bool P1Selected = true;     // boolean to know which name is being clicked on currently
+    bool player1White = true;           // boolean to keep track of who is white
+    bool visible = true;            // boolean to know if 0.5seconds has elapsed
+    string userName = p1name;           // username to save code by not having to switch between p1name and p2name
+
+    while (window.isOpen()) {
+        // do these in main loop, so it constantly happens
+        window.draw(welcomeImage);      // draw background image
+        window.draw(welcomeText);           // draw "CHESS"
+        window.draw(player1Prompt);             // draw name prompt
+        window.draw(player2Prompt);             // draw other name prompt
+        player1Name.setString(p1name);              // set the string of player 1 to be the updated player1 name
+        player1Cursor.setPosition(sf::Vector2f(player1Name.getLocalBounds().width + 880, 95));        // reposition the cursor if the size of the text has changed
+        window.draw(player1Name);           // draw player1 name
+        player2Name.setString(p2name);              // set the string of player 2 to be the updated player2 name
+        player2Cursor.setPosition(sf::Vector2f(player2Name.getLocalBounds().width + 880, 170));
+        window.draw(player2Name);               // draw the name
+        if (clock.getElapsedTime().asSeconds() >= 0.5f) {           // if 0.5 seconds has passed
+            visible = !visible;                     // then the cursor should be visible
+            clock.restart();                        // restart the clock
+        }
+        if ((P1Selected) and (visible)) {           // if player1 name is selected and it has been >0.5 seconds
+            window.draw(player1Cursor);                                                 // draw it
+        } else if ((not P1Selected) and (visible)){     // if player2 is selected, and it has been >0.5 seconds
+            window.draw(player2Cursor);
+        }
+        sf::Vector2i hoverLocal = mouse.getPosition(window);        // get the mouse position
+        if (playButton.getGlobalBounds().contains(hoverLocal.x, hoverLocal.y)) {
+            playButton.setCharacterSize(65);
+        } else {
+            playButton.setCharacterSize(60);
+        }
+        window.draw(playButton);
+        window.draw(credit1);
+        window.draw(credit2);
+        window.draw(wPawn);
+        window.draw(bPawn);
+        window.draw(flipButt);
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {      // if they close the window
+                window.close();
+                return;
+            } else if (event.type == sf::Event::MouseButtonPressed) {       // if they click
+                sf::Vector2i click = mouse.getPosition(window);        // get the mouse position
+                if (player1Prompt.getGlobalBounds().contains(click.x, click.y)) {     // if they click player1's name
+                    cout << click.x << endl;
+                    P1Selected = true;          // player1 is selected
+                    userName = p1name;          // username equals the current player 1 name
+                } else if (player2Prompt.getGlobalBounds().contains(click.x, click.y)) {
+                    P1Selected = false;         // player2 is selected
+                    userName = p2name;          // set username equal to player2 name
+                } else if (playButton.getGlobalBounds().contains(click.x, click.y)) {
+                    user1 = p1name;
+                    user2 = p2name;
+                    user1White = player1White;
+                    return;
+                }
+            } else if (event.type == sf::Event::TextEntered) {      // checks if text was entered
+                if (isalpha(static_cast<char>(event.text.unicode))) {       // checks if the text was a letter
+                    if (userName.size() == 10) {            // if name length is at 10
+                        continue;
+                    } else {
+                        userName += static_cast<char>(event.text.unicode);      // add current char to userName
+                    }
+                }
+            } else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::BackSpace) {
+                    if ((userName.size() == 0) or (userName.size() == 1)) {
+                        userName = "";
+                    } else {
+                        userName = userName.substr(0, userName.size() - 1);
+                    }
+                }
+            }
+        }
+        if (P1Selected) {
+            p1name = userName;
+        } else {
+            p2name = userName;
+        }
+        window.display();
+    }
 }
 
